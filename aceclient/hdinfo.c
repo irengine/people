4294f89eda2f4b5e4d0bd73a386696b1eeaaa912
greg ma <gregma@ubuntu.(none)>
2012-06-23 12:24:09 +0800

    code update

--- aceclient/hdinfo.c
+++ aceclient/hdinfo.c
@@ -22,7 +22,27 @@ int main(int argc, const char * argv[])
 
   if(!ioctl(fd, HDIO_GET_IDENTITY, &id))
   {
-    printf("get hd serial #:%s\n", id.serial_no);
+    int m = sizeof(id.serial_no);
+    char buff[100];
+    memcpy(buff, id.serial_no, sizeof(id.serial_no));
+    char * ptr = buff;
+    ptr[m] = 0;
+    int i;
+    for (i = 0; i < m - 1; ++i)
+    {
+      if (*ptr == ' ' || *ptr == '\t')
+        ++ptr;
+      else
+        break;
+    }
+    int len = strlen(ptr);
+    while((len > 0) && (*(ptr + len - 1) == ' ' || *(ptr + len - 1) == '\t' || *(ptr + len - 1) == '\r' || *(ptr + len - 1) == '\n'))
+    {
+      *(ptr + len -1) = 0;
+      --len;
+    }
+
+    printf("get hd serial #:%s\n", ptr);
     const char * fn = "/tmp/tmpv9397";
     int fd2 = open(fn, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (fd2 < 0)
@@ -30,8 +50,8 @@ int main(int argc, const char * argv[])
       perror(fn);
       return 3;
     }
-    int n = strlen(id.serial_no);
-    write(fd2, id.serial_no, n);
+    int n = strlen(ptr);
+    write(fd2, ptr, n);
     close(fd2);
     rename(fn, argv[2]);
   } else
